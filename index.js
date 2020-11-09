14bc53c18b5057d6140d61d8e6eaa6ce11c5942d
Paul Mossmann <49226148+paulmossmann@users.noreply.github.com>
2020-11-09 14:01:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
-let hauteur = 600 / 8;
-let largeur = 800 / 10;
-let carreTaille = 10;
-
-// Creation grille a 2 dimentions
-let grille = new Array(largeur);
-for (var i = 0; i < largeur; i++) {
-    grille[i] = new Array(hauteur);
-}
-
-// Recuperation du context
+// Recuperation du contexte
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
-ctx.fillStyle = "#fff";
-// Initialisation de la grille
-var k = 0;
-var l = 0;
-for (var i = 0; i < grille.length; i += 1) {
-    for (var j = 0; j < grille[i].length; j += 1) {
-        grille[i][j] = new Pixel(k, l, carreTaille, carreTaille);
-        grille[i][j].drawPixel();
-        l += 11;
-    }
-    l = 0;
-    k += 11;
+
+const resolution = 10;
+canvas.width = 800;
+canvas.height = 800;
+const COLONNES = canvas.width / resolution;
+const LIGNES = canvas.height / resolution;
+
+//Fonction qui permet de creer un tableau a deux dimensions
+function creerTableau2d() {
+    return new Array(COLONNES).fill(null)
+        .map(() => new Array(LIGNES).fill(null)
+            .map(() => Math.floor(Math.random() * 2)));
 }
 
-document.addEventListener("keydown", function (event) {
-    console.log(event);
-    if (event.key == 'e') {
-        console.log('e pressed');
-        test();
-    }
-})
+//Creation de la grille
+let grille = creerTableau2d();
+requestAnimationFrame(update);
 
-function test() {
-    for (var i = 1; i < grille.length; i++) {
-        for (var j = 1; j < grille[i].length; j++) {
-            var nombreVoisin = 0;
-            // Boucle qui calcul le nombre de voisin
-            for (var k = -1; k <= 1; k++) {
-                for (var l = -1; l <= 1; l++) {
-                    try {
-                        if (grille[k][l].alive == true) {
-                            nombreVoisin++;
-                        }
+function update() {
+  grille = nouvelleGeneration(grille);
+  dessinerGrille(grille);
+  requestAnimationFrame(update);
+}
+
+function nouvelleGeneration(grille) {
+    const nouvelleGeneration = grille.map(arr => [...arr]);
+
+    for (let col = 0; col < grille.length; col++) {
+        for (let row = 0; row < grille[col].length; row++) {
+            const cell = grille[col][row];
+            let nombreVoisin = 0;
+            for (let i = -1; i < 2; i++) {
+                for (let j = -1; j < 2; j++) {
+                    if (i === 0 && j === 0) {
+                        continue;
                     }
-                    catch {
+                    const x_cell = col + i;
+                    const y_cell = row + j;
 
-                    }
-                    finally{
-
+                    if (x_cell >= 0 && y_cell >= 0 && x_cell < COLONNES && y_cell < LIGNES) {
+                        const currentNeighbour = grille[col + i][row + j];
+                        nombreVoisin += currentNeighbour;
                     }
                 }
             }
-            nombreVoisin--;
-            if (grille[i][j].alive == true && nombreVoisin < 2) {
-                grille[i][j].alive = false;
-                grille[i][j].drawPixel();
-            }
-            else if (grille[i][j].alive == true && nombreVoisin > 3) {
-                grille[i][j].alive = false;
-                grille[i][j].drawPixel();
-            }
 
-            else if (grille[i][j].alive == false && nombreVoisin == 3) {
-                grille[i][j].alive = true;
-                grille[i][j].drawPixel();
+            // rules
+            if (cell === 1 && nombreVoisin < 2) {
+                nouvelleGeneration[col][row] = 0;
+            } else if (cell === 1 && nombreVoisin > 3) {
+                nouvelleGeneration[col][row] = 0;
+            } else if (cell === 0 && nombreVoisin === 3) {
+                nouvelleGeneration[col][row] = 1;
             }
+        }
+    }
+    return nouvelleGeneration;
+}
+function dessinerGrille(grille) {
+    for (let col = 0; col < grille.length; col++) {
+        for (let row = 0; row < grille[col].length; row++) {
+            const cell = grille[col][row];
+            ctx.beginPath();
+            ctx.rect(col * resolution, row * resolution, resolution, resolution);
+            ctx.fillStyle = cell ? 'black' : 'white';
+            ctx.fill();
+            ctx.stroke();
         }
     }
 }
