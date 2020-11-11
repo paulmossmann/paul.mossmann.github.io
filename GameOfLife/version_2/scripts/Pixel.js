2e138e93c23f9e5f47d07161e75cdc59c7f7f95c
Paul Mossmann <paul.mossmann@laposte.net>
2020-11-10 20:32:31 -0500

    debut version 2

---
+++ GameOfLife/version_2/scripts/Pixel.js
@@ -0,0 +1,9 @@
+class Pixel{
+    constructor(){
+        this.posX = 1;
+        this.posY = 1;
+        this.alive = Math.floor(Math.random() * 2);
+        this.hauteur = 1;
+        this.largeur = 1;
+    }
+}
---
+++ GameOfLife/version_2/scripts/index.js
@@ -0,0 +1,92 @@
+// Recuperation du contexte
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const resolution = 10;
+canvas.width = 800;
+canvas.height = 800;
+const COLONNES = canvas.width / resolution;
+const LIGNES = canvas.height / resolution;
+
+//Fonction qui permet de creer un tableau a deux dimensions
+function creerTableau2d() {
+    var tableau = new Array(COLONNES);
+    for(var i = 0; i < tableau.length; i++){
+        tableau[i] = new Array(LIGNES);
+    }
+    return tableau;
+}
+
+function remplirGrille(grille){
+    for(var i = 0; i < grille.length; i++){
+        for(var j = 0; j < grille[i].length; j++){
+            grille[i][j] = new Pixel();
+        }
+    }
+}
+
+//Creation de la grille
+let grille = creerTableau2d();
+grille = remplirGrille(grille);
+requestAnimationFrame(update);
+
+function update() {
+  grille = nouvelleGeneration(grille);
+  dessinerGrille(grille);
+  requestAnimationFrame(update);
+}
+
+function copieTabbleay(tableauBase, tableauCopie){
+    for(var i = 0; i < tableauBase.length; i++ ){
+        for(var j = 0; j < tableauBase[i].length; j++){
+            tableauCopie[i][j] = tableauBase[i][j];
+        }
+    }
+    return tableauCopie;
+}
+
+function nouvelleGeneration(grille) {
+    const nouvelleGeneration = creerTableau2d();
+    nouvelleGeneration = remplirGrille(grille, nouvelleGeneration);
+    for (let col = 0; col < grille.length; col++) {
+        for (let row = 0; row < grille[col].length; row++) {
+            const cell = grille[col][row].alive;
+            let nombreVoisin = 0;
+            for (let i = -1; i < 2; i++) {
+                for (let j = -1; j < 2; j++) {
+                    if (i === 0 && j === 0) {
+                        continue;
+                    }
+                    const x_cell = col + i;
+                    const y_cell = row + j;
+                    if (x_cell >= 0 && y_cell >= 0 && x_cell < COLONNES && y_cell < LIGNES) {
+                        const currentNeighbour = grille[col + i][row + j].alive;
+                        nombreVoisin += currentNeighbour;
+                    }
+                }
+            }
+
+            // rules
+            if (cell === 1 && nombreVoisin < 2) {
+                nouvelleGeneration[col][row].alive = 0;
+            } else if (cell === 1 && nombreVoisin > 3) {
+                nouvelleGeneration[col][row].alive = 0;
+            } else if (cell === 0 && nombreVoisin === 3) {
+                nouvelleGeneration[col][row].alive = 1;
+            }
+        }
+    }
+    return nouvelleGeneration;
+}
+function dessinerGrille(grille) {
+    for (let col = 0; col < grille.length; col++) {
+        for (let row = 0; row < grille[col].length; row++) {
+            const cell = grille[col][row].alive;
+            ctx.beginPath();
+            ctx.rect(col * resolution, row * resolution, resolution, resolution);
+            ctx.fillStyle = cell ? 'black' : 'white';
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
+}
